60cfbe8c104b96140cad43b532b3e1fb48a1913c
James Pancoast <jpancoast@gmail.com>
2018-11-03 20:26:22 -0600

    Initial commit, the AWS consul packer template is working.

---
+++ aws/README.md
@@ -0,0 +1,4 @@
+Requirements:
+VPC tagged with Name: build, IGW attached and routed too.
+Subnet in that VPC tagged with Name: build and auto assign public IP set to true.
+Probably should use the terraform registry vpc module!
---
+++ aws/terraform/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    description = "Region to install the VPC into."
+    type = "string"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR to use for the VPC."
+    type = "string"
+}
+
+#variable "subnet_cidr" {
+#    description = "CIDR to use for the build subnet."
+#    type = "string"
+#}
