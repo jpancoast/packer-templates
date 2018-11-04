8cf4ae6a6ecc9b9ed57a05f97125055f63c7b49e
James Pancoast <jpancoast@gmail.com>
2018-11-03 20:26:48 -0600

    formatted

--- aws/terraform/vpc/variables.tf
+++ aws/terraform/vpc/variables.tf
@@ -1,14 +1,15 @@
 variable "region" {
-    description = "Region to install the VPC into."
-    type = "string"
+  description = "Region to install the VPC into."
+  type        = "string"
 }
 
 variable "vpc_cidr" {
-    description = "CIDR to use for the VPC."
-    type = "string"
+  description = "CIDR to use for the VPC."
+  type        = "string"
 }
 
 #variable "subnet_cidr" {
 #    description = "CIDR to use for the build subnet."
 #    type = "string"
 #}
+
